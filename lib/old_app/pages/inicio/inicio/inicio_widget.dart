91063c917514f62a71fc913405b0900cccfa8e24
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-01-23 19:55:21 +0000

    Updating to latest FlutterFlow output.

--- lib/old_app/pages/inicio/inicio/inicio_widget.dart
+++ lib/old_app/pages/inicio/inicio/inicio_widget.dart
@@ -1250,7 +1250,7 @@ class _InicioWidgetState extends State<InicioWidget> {
                                                         30.0, 0.0, 30.0, 0.0),
                                                 child: Builder(
                                                   builder: (context) {
-                                                    final gridListarDes =
+                                                    final gridListarDois =
                                                         FFAppState()
                                                             .eventosListadosDestaqueDois
                                                             .map((e) => e)
@@ -1267,12 +1267,12 @@ class _InicioWidgetState extends State<InicioWidget> {
                                                       scrollDirection:
                                                           Axis.vertical,
                                                       itemCount:
-                                                          gridListarDes.length,
+                                                          gridListarDois.length,
                                                       itemBuilder: (context,
-                                                          gridListarDesIndex) {
-                                                        final gridListarDesItem =
-                                                            gridListarDes[
-                                                                gridListarDesIndex];
+                                                          gridListarDoisIndex) {
+                                                        final gridListarDoisItem =
+                                                            gridListarDois[
+                                                                gridListarDoisIndex];
                                                         return Container(
                                                           width: 150.0,
                                                           height: 150.0,
@@ -1322,40 +1322,40 @@ class _InicioWidgetState extends State<InicioWidget> {
                                                                             ModalEventoWidget(
                                                                           nomeTitulo:
                                                                               getJsonField(
-                                                                            gridListarDesItem,
+                                                                            gridListarDoisItem,
                                                                             r'''$.descricao''',
                                                                           ).toString(),
                                                                           exclusivoGiatnts:
                                                                               true,
                                                                           dataEvento:
                                                                               getJsonField(
-                                                                            gridListarDesItem,
+                                                                            gridListarDoisItem,
                                                                             r'''$.data''',
                                                                           ).toString(),
                                                                           localEvento:
                                                                               getJsonField(
-                                                                            gridListarDesItem,
+                                                                            gridListarDoisItem,
                                                                             r'''$.local''',
                                                                           ).toString(),
                                                                           sobreEvento:
                                                                               'teste',
                                                                           fotoBase64:
                                                                               getJsonField(
-                                                                            gridListarDesItem,
+                                                                            gridListarDoisItem,
                                                                             r'''$.banner''',
                                                                           ).toString(),
                                                                           dataEventoFim:
                                                                               getJsonField(
-                                                                            gridListarDesItem,
+                                                                            gridListarDoisItem,
                                                                             r'''$.dataFim''',
                                                                           ).toString(),
                                                                           idEvento:
                                                                               getJsonField(
-                                                                            gridListarDesItem,
+                                                                            gridListarDoisItem,
                                                                             r'''$.id''',
                                                                           ).toString(),
                                                                           jaIngressou: getJsonField(
-                                                                                    gridListarDesItem,
+                                                                                    gridListarDoisItem,
                                                                                     r'''$.ja_inscrito''',
                                                                                   ) !=
                                                                                   null
@@ -1416,12 +1416,12 @@ class _InicioWidgetState extends State<InicioWidget> {
                                                                                           height: double.infinity,
                                                                                           base64Image: valueOrDefault<String>(
                                                                                             getJsonField(
-                                                                                                      gridListarDesItem,
+                                                                                                      gridListarDoisItem,
                                                                                                       r'''$.banner''',
                                                                                                     ) !=
                                                                                                     null
                                                                                                 ? getJsonField(
-                                                                                                    gridListarDesItem,
+                                                                                                    gridListarDoisItem,
                                                                                                     r'''$.banner''',
                                                                                                   ).toString()
                                                                                                 : 'iVBORw0KGgoAAAANSUhEUgAAAREAAAC4CAMAAADzLiguAAAANlBMVEXp7vG6vsHs8fS2ur3c4eTU2dzm6u3P1Ne4vL/u8/a4vL67v8G0ubzDx8rY3eDEyMvh5unKz9Izr04MAAADb0lEQVR4nO2c63KrIBRGFY1CY4x5/5c93nKiICZGGOvuWj86adowYc0HWxgxSQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAOC3oiJwdJ/2oJr6Epy6Sc4qxeTXKtNPfoYfP9NXDj//f0xfv97oX2X6cU4l5pGl6TWNgdbF0b37AnPXUWwMVEd37wvqLKKQNnzm6A5uxcRMSEuWH93DrTRV/8XbaVBnQdFj9u4nm13Vpc+ILk3wy5FCn3LYqHL43hG+9ti0PqmRCNGO2HRMVJlGNqIx8mpakpEQyzRBRlSSd+u0vT0RY8Tkt6rq1mnXcl9fpBjp130DOt2Vk8HI9exG1G16VV81u5qWkBF7Ibxn6SrDSF5ZC7UdqxIRRoyzcZR9P25EGCnsiLRLwK87JMGIqt3NkjdL15VdQxFGSkfIm+v7Irt7jUmovm0f3B3o1Q7pVHuViMjIZeOo6aYdffP8hwQjSePuQq+U33Ee9ikRYcQ4tSar/Z996vMoEWHkue31wTSiJpV6WYkII4myjFS5rz/FdIAtKpFhxJpJqod3Xp3POEtKJFTf7vdGv2KSeYU4F7cLSoRkJFHJvRqcZDr3CnFrkntdIsVIW3CK8tam/ZEbb1+ckrSUEjlG2jeNUsbvw10PjimZf0KSkfVPLAyZxYHzV4woT0LcgSOk1rylWLu7YpaSv5KR9ftvpin5G0ZWhoyjRKIRU1tvF9XbO5JeSgQaMXU1nyrfJmSmRJ6RVkia3iZ/+CAhaVdcRiXijPRCpoPAex3iSYm06qvq+Q7ZZ0NmVDIxIiYjTyGdkv5vG4SINGIm9/32Kfl4yAg1YuppIlolWxIi0Yip7R2ybTdGizNiC9mMFlZr1O6zA8Iysjsh0oy0ZXf36SNRRsxlU1WRb8RcQpw/EmSkuw4JcGJPkJE6wJBJJVXfxXuMdho5d0YwkmDEBSM2GLGJboRaYxs5d0YSjNgZeVRBjoNXYowkTR6GsWkBRgI3jRG7aYzYTWPEbvqkRqI97sCc1MiwaaYfSRGa/JzPH3k+oyYNciEyZ2j4dE8Ac49vhmXHYdCjyOM+68p3QusXY8owm6uL6LPNqz0RlWTXozv3Haq5R5hXW66XMyakxwRb400p/IcNAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA4FD+AZS0NBe99dfKAAAAAElFTkSuQmCC',
@@ -1443,7 +1443,7 @@ class _InicioWidgetState extends State<InicioWidget> {
                                                                                             alignment: AlignmentDirectional(0.0, 0.0),
                                                                                             child: Text(
                                                                                               getJsonField(
-                                                                                                gridListarDesItem,
+                                                                                                gridListarDoisItem,
                                                                                                 r'''$.descricao''',
                                                                                               ).toString(),
                                                                                               style: FlutterFlowTheme.of(context).bodyMedium.override(
@@ -1460,7 +1460,7 @@ class _InicioWidgetState extends State<InicioWidget> {
                                                                                               dateTimeFormat(
                                                                                                 'E. dd MMM • HH:mm',
                                                                                                 functions.strDataParaDateTime(getJsonField(
-                                                                                                  gridListarDesItem,
+                                                                                                  gridListarDoisItem,
                                                                                                   r'''$.data''',
                                                                                                 ).toString()),
                                                                                                 locale: FFLocalizations.of(context).languageCode,
